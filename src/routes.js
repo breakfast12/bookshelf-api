53e3febb1a8adcc9ad65f1cfa64adf1a20da6a4f
Freza Ridho Septyawan <fresaridhom@gmail.com>
2022-04-14 00:14:31 +0700

    bookshelf-api

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+const { addBookHandler, getAllBooksHandler } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  },
+];
+
+module.exports = routes;
